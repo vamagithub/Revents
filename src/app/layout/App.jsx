2425569b1c687db3fe34fee4c9ee28032f193272
Vama Mehta <vama.mehta@inqubit.in>
2022-08-05 14:36:35 +0530

    feat - Initial Commit - Project Structure Created.

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,12 @@
+
+import './style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Re-Vents</h1>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './app/layout/style.css';
+import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+const rootEl = ReactDOM.createRoot(document.getElementById('root'));
+function render(){
+  rootEl.render(
+    <App/>
+  )
+}
+
+// _____________________________ ?? HOT MODULE REPLACEMENT ___________________________________
+
+
+if(module.hot){
+  module.hot.accept('./app/layout/App', function(){
+    setTimeout(render);
+  })
+}
+
+render();
+
+// ______________________________ ?? HOT MODULE REPLACEMENT ___________________________________
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log("PERFORMANCE"));
